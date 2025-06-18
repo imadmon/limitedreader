95c1ccc72a7ac279367a465c13c73a50020eb81d
imadmon <i101madmon@gmail.com>
2025-06-18 13:52:21 +0300

    support option context

--- option.go
+++ option.go
@@ -1,9 +1,17 @@
 package limitedreader
 
+import "context"
+
 type Option func(*LimitedReader)
 
 func WithClock(c Clock) Option {
 	return func(lr *LimitedReader) {
 		lr.clock = c
+	}
+}
+
+func WithContext(ctx context.Context) Option {
+	return func(lr *LimitedReader) {
+		lr.ctx = ctx
 	}
 }
